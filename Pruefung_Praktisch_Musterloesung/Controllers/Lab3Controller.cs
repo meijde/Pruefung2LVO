e6ee01da2932a445aaa035d18008bdd349170c82
Lars Vognstrup <lars.vognstrup@bluewin.ch>
2018-01-19 11:13:13 +0100

    Aufgabe 3 fertig Theorie

--- Pruefung_Praktisch_Musterloesung/Controllers/Lab3Controller.cs
+++ Pruefung_Praktisch_Musterloesung/Controllers/Lab3Controller.cs
@@ -20,6 +20,8 @@ namespace Pruefung_Praktisch_Musterloesung.Controllers
         * 2) Stored XSS -       Es wird ein Script File in zb einem Kommentar auf einem Blog publiziert. Dieses wird dann in die DB gespeichert.
         *    SQL Injection -    Es wird verscuht SQL in inputfelder der Website einzuschleusen, sodass diese ausgeführt werden in einer DB Abfrage im Code.
         *       --> Es kann alles aus der DB geholt werden, auch sensitive Daten!
+        *       --> DB kann abgeschossen werden wenn zb. ein Stern bzw. All eingeschleust wird, sodass die DB soviele Daten schaufeln muss dass sie abstürzt.
+        *       
         * 3) 
         * 
         * 
